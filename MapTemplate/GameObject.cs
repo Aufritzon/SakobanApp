8ad13a8d39958f50eb7331a38ce487b16a3db0bb
Aufritzon <w.fritzon@hotmail.se>
2022-10-04 13:57:32 +0200

    first commit

---
+++ MapTemplate/GameObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapTemplate
+{
+    class GameObject
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public string mapMarker { get; set; }
+
+        public GameObject(int x, int y, string mapMarker)
+        {
+            X = x;
+            Y = y;
+            this.mapMarker = mapMarker;
+        }
+    }
+}
