fac90c9c563e052115db9f10418dc5e26f4a8ddd
Aufritzon <w.fritzon@hotmail.se>
2022-10-18 15:41:19 +0200

    changes

--- MapTemplate/Map.cs
+++ MapTemplate/Map.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +10,7 @@ namespace MapTemplate
 {
     class Map
     {
-        string[][] map =
+        private static string[][] map2 =
         {
             new string[] { ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
             new string[] { ".", ".", ".", ".", ".", ".", ".", ".", ".", "." },
@@ -23,7 +24,20 @@ namespace MapTemplate
             new string[] { ".", ".", ".", ".", ".", ".", ".", ".", ".", "." }
         };
 
-        string[][] tempMap;
+        private static string[][] map =
+        {
+            new string[] { "w", "w", "w", "w", "w", "w", "w", "w", "w", "w" },
+            new string[] { "w", ".", ".", ".", ".", ".", ".", ".", ".", "w" },
+            new string[] { "w", ".", ".", ".", ".", "t", ".", ".", ".", "w" },
+            new string[] { "w", ".", "w", ".", ".", ".", ".", ".", ".", "w" },
+            new string[] { "w", ".", "w", ".", ".", ".", ".", ".", ".", "w" },
+            new string[] { "w", ".", "w", ".", ".", ".", ".", "t", ".", "w" },
+            new string[] { "w", ".", ".", ".", ".", "w", "w", ".", ".", "w" },
+            new string[] { "w", ".", ".", ".", ".", ".", ".", ".", ".", "w" },
+            new string[] { "w", ".", ".", ".", ".", ".", ".", ".", ".", "w" },
+            new string[] { "w", "w", "w", "w", "w", "w", "w", "w", "w", "w" }
+        };
+
 
         public Map()
         {
@@ -34,13 +48,25 @@ namespace MapTemplate
         // then prints it.
         public void PrintMap(List<GameObject> objects)
         {
-            tempMap = (string[][])map.Clone();
-            foreach(GameObject obj in objects)
+            var tempMap = CloneMap(map);
+
+            foreach (GameObject obj in objects)
             {
                 tempMap[obj.Y][obj.X] = obj.mapMarker;
             }
 
             PrintMap(tempMap);
+        }
+
+        private string[][] CloneMap(string[][] map)
+        {
+            string[][] mapClone = new string[map.Length][];
+
+            for (int i = 0; i < map.Length; i++)
+            {
+                mapClone[i] = (string[]) map[i].Clone();
+            }
+            return mapClone;
         }
 
         // This function prints a map that is given to it.
@@ -56,10 +82,27 @@ namespace MapTemplate
             }
         }
 
+        public List<GameObject> GetMapTargets()
+        {
+            List<GameObject> targets = new List<GameObject>();
+            for (int y = 0; y < map.Length; y++)
+            {
+                for (int x = 0; x < map[y].Length; x++)
+                {
+                    if (map[y][x].Equals("t"))
+                    {
+                        targets.Add(new GameObject(x,y,"t"));
+                    }
+                }
+            }
+
+            return targets;
+        }
+
         // Returns what kind of tile it is via an x and y value.
         public string ReturnTile(int x, int y)
         {
-            if (x < 0 || y < 0 || x > map[y].Length || y > map.Length)
+            if (x < 0 || y < 0 || x == map[0].Length || y == map.Length)
             {
                 return "";
             }
